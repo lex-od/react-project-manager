9f749170edf6c91dbd026397133641c372a69791
Oleksandr Myrvoda <mirvoda2@gmail.com>
2021-05-07 18:01:16 +0300

    datePicker

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -3,3 +3,4 @@ export { default as NewItemModal } from './NewItemModal/NewItemModal';
 export { default as ViewWrapper } from './ViewWrapper/ViewWrapper';
 export { default as Sidebar } from './Sidebar/Sidebar';
 export { default as Spinner } from './Spinner/Spinner';
+export { default as DatePicker } from './DatePicker/DatePicker';
