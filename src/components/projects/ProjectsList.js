f68cb2efc4b789e0f02359e74604a324b5f202ba
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-05-07 17:19:23 +0300

    добавляет разметку

--- src/components/projects/ProjectsList.js
+++ src/components/projects/ProjectsList.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import projects from './data';
+import deleteBtn from '../../assets/icons/deleteSprite.svg';
+import addBtn from '../../assets/icons/addBtnSprite.svg';
+import css from './Projects.module.scss';
+
 
 const ProjectsList = () => {
+
     return (
-        <>
-            <h1>Проекти</h1>
-        </>
+        <div className={css.projectsContainer}>
+            <h1 className={css.projectsTitle}>Проекти</h1>
+            <ul className={css.projectsList}>
+                {projects.map(project => (
+                    <li className={css.projectsListItem}>
+                        <div className={css.projectsItemContainer}>
+                            <h2 className={css.projectsListItemTitle}>{project.title}</h2>
+                            <p className={css.projectsListItemDescr}>{project.description}</p>
+                            <button className={css.projectsListItemButton}>
+                                <svg className={css.icon}>
+                                    <use href={deleteBtn + '#icon-trash'}></use>
+                                </svg>
+                            </button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+            <button className={css.addBtn}>
+                <svg className={css.addBtnIcon}>
+                    <use href={addBtn + '#icon-addBtn'}></use>
+                </svg>
+            </button>
+        </div>
     );
 }
 
