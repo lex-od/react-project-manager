fcbfc8f2a9ba1cf7270dc50135bc222d631b66a0
taijayson <71400805+taijayson@users.noreply.github.com>
2021-05-09 20:07:12 +0300

    main html and styles added

--- src/components/sprints/SprintsContent/SprintsContent.jsx
+++ src/components/sprints/SprintsContent/SprintsContent.jsx
@@ -2,92 +2,137 @@ import styles from './SprintsContent.module.scss';
 import spriteSearch from '../../../assets/icons/searchIcon.svg';
 import spriteText from '../../../assets/icons/sprintsText.svg';
 import spriteDelete from '../../../assets/icons/sprintsDelete.svg';
-import spriteLeft from '../../../assets/icons/sprintsArrowLeft.svg';
-import spriteRight from '../../../assets/icons/sprintsArrowRight.svg';
+import spriteAddPeople from '../../../assets/icons/addPeople.svg';
 
 export default function SprintsContent() {
     return (
         <div className={styles.wrap}>
-            <div className={styles.section}>
-                <div className={styles.toggleWrap}>
-                    <svg className={styles.leftSvg}>
-                        <use href={spriteLeft + '#icon-left'}></use>
-                    </svg>
-                    <svg className={styles.rightSvg}>
-                        <use href={spriteRight + '#icon-right'}></use>
-                    </svg>
-                    <span className={styles.date}>08.08.2020</span>
-                </div>
-
-                <form className={styles.form}>
-                    <input className={styles.input} type="text"></input>
-                    <svg className={styles.inputSvg}>
-                        <use href={spriteSearch + '#icon-search'}></use>
-                    </svg>
-                </form>
+            <div className={styles.sprintHead}>
+                <h1 className={styles.title}>Project 1</h1>
+                <svg className={styles.textSvg}>
+                    <use href={spriteText + '#icon-text'}></use>
+                </svg>
             </div>
-            <div>
-                <div className={styles.sprintHead}>
-                    <h1 className={styles.title}>Sprint Burndown Chart 1</h1>
-                    <svg className={styles.textSvg}>
-                        <use href={spriteText + '#icon-text'}></use>
-                    </svg>
-                </div>
-                <ul className={styles.sprintsList}>
-                    <li className={styles.sprintCard}>
-                        <h2 className={styles.sprintCardHead}>
-                            KN-1 Configure project
-                        </h2>
-                        <div className={styles.sprintItem}>
-                            <span className={styles.sprintItemText}>
-                                Заплановано годин
-                            </span>
-                            <span className={styles.sprintItemText}>8</span>
-                        </div>
-                        <div className={styles.sprintItem}>
-                            <span className={styles.sprintItemText}>
-                                Витрачено год / день
-                            </span>
-                            <span className={styles.sprintItemText}>6</span>
-                        </div>
-                        <div className={styles.sprintItem}>
-                            <span className={styles.sprintItemText}>
-                                Витрачено годин
-                            </span>
-                            <span className={styles.sprintItemText}>0</span>
-                        </div>
-                        <svg className={styles.deleteSvg}>
-                            <use href={spriteDelete + '#icon-delete'}></use>
-                        </svg>
-                    </li>
-                    <li className={styles.sprintCard}>
-                        <h2 className={styles.sprintCardHead}>
-                            KN-1 Configure project
-                        </h2>
-                        <div className={styles.sprintItem}>
-                            <span className={styles.sprintItemText}>
-                                Заплановано годин
-                            </span>
-                            <span className={styles.sprintItemText}>8</span>
-                        </div>
-                        <div className={styles.sprintItem}>
-                            <span className={styles.sprintItemText}>
-                                Витрачено год / день
-                            </span>
-                            <span className={styles.sprintItemText}>6</span>
-                        </div>
-                        <div className={styles.sprintItem}>
-                            <span className={styles.sprintItemText}>
-                                Витрачено годин
-                            </span>
-                            <span className={styles.sprintItemText}>0</span>
-                        </div>
-                        <svg className={styles.deleteSvg}>
-                            <use href={spriteDelete + '#icon-delete'}></use>
-                        </svg>
-                    </li>
-                </ul>
+            <div className={styles.sprintDescription}>
+                <p className={styles.sprintDescriptionText}>
+                    Короткий опис проекту, якщо він є, розміщуєтсья тут. Ширина
+                    тектового блоку
+                </p>
             </div>
+            <div className={styles.addPeopleWrap}>
+                <svg className={styles.addPeopleSvg}>
+                    <use href={spriteAddPeople + '#icon-addPeople'}></use>
+                </svg>
+                <button type="button" className={styles.addPeopleBtn}>
+                    Додати людей
+                </button>
+            </div>
+            <ul className={styles.sprintsList}>
+                <li className={styles.sprintCard}>
+                    <h2 className={styles.sprintCardHead}>
+                        Sprint Burndown Chart 1
+                    </h2>
+                    <div className={styles.sprintItem}>
+                        <span className={styles.sprintItemText}>
+                            Дата початку
+                        </span>
+                        <span className={styles.sprintItemText}>10 Лип</span>
+                    </div>
+                    <div className={styles.sprintItem}>
+                        <span className={styles.sprintItemText}>
+                            Дата закінченя
+                        </span>
+                        <span className={styles.sprintItemText}>22 Лип</span>
+                    </div>
+                    <div className={styles.sprintItem}>
+                        <span className={styles.sprintItemText}>
+                            Тривалість
+                        </span>
+                        <span className={styles.sprintItemText}>226</span>
+                    </div>
+                    <svg className={styles.deleteSvg}>
+                        <use href={spriteDelete + '#icon-delete'}></use>
+                    </svg>
+                </li>
+                <li className={styles.sprintCard}>
+                    <h2 className={styles.sprintCardHead}>
+                        Sprint Burndown Chart 1
+                    </h2>
+                    <div className={styles.sprintItem}>
+                        <span className={styles.sprintItemText}>
+                            Дата початку
+                        </span>
+                        <span className={styles.sprintItemText}>10 Лип</span>
+                    </div>
+                    <div className={styles.sprintItem}>
+                        <span className={styles.sprintItemText}>
+                            Дата закінченя
+                        </span>
+                        <span className={styles.sprintItemText}>22 Лип</span>
+                    </div>
+                    <div className={styles.sprintItem}>
+                        <span className={styles.sprintItemText}>
+                            Тривалість
+                        </span>
+                        <span className={styles.sprintItemText}>226</span>
+                    </div>
+                    <svg className={styles.deleteSvg}>
+                        <use href={spriteDelete + '#icon-delete'}></use>
+                    </svg>
+                </li>
+                <li className={styles.sprintCard}>
+                    <h2 className={styles.sprintCardHead}>
+                        Sprint Burndown Chart 1
+                    </h2>
+                    <div className={styles.sprintItem}>
+                        <span className={styles.sprintItemText}>
+                            Дата початку
+                        </span>
+                        <span className={styles.sprintItemText}>10 Лип</span>
+                    </div>
+                    <div className={styles.sprintItem}>
+                        <span className={styles.sprintItemText}>
+                            Дата закінченя
+                        </span>
+                        <span className={styles.sprintItemText}>22 Лип</span>
+                    </div>
+                    <div className={styles.sprintItem}>
+                        <span className={styles.sprintItemText}>
+                            Тривалість
+                        </span>
+                        <span className={styles.sprintItemText}>226</span>
+                    </div>
+                    <svg className={styles.deleteSvg}>
+                        <use href={spriteDelete + '#icon-delete'}></use>
+                    </svg>
+                </li>
+                <li className={styles.sprintCard}>
+                    <h2 className={styles.sprintCardHead}>
+                        Sprint Burndown Chart 1
+                    </h2>
+                    <div className={styles.sprintItem}>
+                        <span className={styles.sprintItemText}>
+                            Дата початку
+                        </span>
+                        <span className={styles.sprintItemText}>10 Лип</span>
+                    </div>
+                    <div className={styles.sprintItem}>
+                        <span className={styles.sprintItemText}>
+                            Дата закінченя
+                        </span>
+                        <span className={styles.sprintItemText}>22 Лип</span>
+                    </div>
+                    <div className={styles.sprintItem}>
+                        <span className={styles.sprintItemText}>
+                            Тривалість
+                        </span>
+                        <span className={styles.sprintItemText}>226</span>
+                    </div>
+                    <svg className={styles.deleteSvg}>
+                        <use href={spriteDelete + '#icon-delete'}></use>
+                    </svg>
+                </li>
+            </ul>
         </div>
     );
 }
--- src/components/sprints/SprintsSideContent/SprintsSideContent.jsx
+++ src/components/sprints/SprintsSideContent/SprintsSideContent.jsx
@@ -1,1 +1,21 @@
-// import styles from './SprintsSidebar.module.scss';
+import styles from './SprintsSideContent.module.scss';
+import spriteGoBack from '../../../assets/icons/goBackArrow.svg';
+
+export default function SprintsContent() {
+    return (
+        <div className={styles.asideWrap}>
+            <div className={styles.asideBack}>
+                <svg className={styles.backSvg}>
+                    <use href={spriteGoBack + '#icon-goBack'}></use>
+                </svg>
+                <span className={styles.asideBackText}>Показати проекти</span>
+            </div>
+            <ul className={styles.asideSprintsList}>
+                <li className={styles.asideSprintsItem}>
+                    <div className={styles.asideSprintsIcon}></div>
+                    <span className={styles.asideSprintsName}>Project 1</span>
+                </li>
+            </ul>
+        </div>
+    );
+}
