9b379d4f359bc4640a16ad6f3adb3245b4d9c204
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-05-11 12:18:09 +0300

    добавляет контейнер для AddButton

--- src/components/common/addButton/AddButton.jsx
+++ src/components/common/addButton/AddButton.jsx
@@ -5,13 +5,13 @@ import css from './AddButton.module.scss';
 
 const AddButton = ({onClick}) => {
     return (
-        <>
+        <div className={css.addBtnContainer}>
             <button type='button' className={css.addBtn} onClick={onClick}>
                 <svg className={css.addBtnIcon}>
                     <use href={addBtn + '#icon-addBtn'}></use>
                 </svg>            
             </button>
-        </>
+        </div>
     );
 }
 
