e64aef545fff5899c788ed545bdf5ac5bce4124f
Aleksey Yesin <esinru@gmail.com>
2021-05-11 22:29:57 +0300

    Добавляет 3 модалки во вьюху спринтов

--- src/components/sprints/SprintsSideContent/SprintsSideContent.jsx
+++ src/components/sprints/SprintsSideContent/SprintsSideContent.jsx
@@ -1,9 +1,18 @@
 import styles from './SprintsSideContent.module.scss';
 import spriteGoBack from '../../../assets/icons/goBackArrow.svg';
+import { useState } from 'react';
+import AddButton from '../../common/addButton/AddButton';
+import { NewItemModal, NewProjectForm } from '../../common';
 
 export default function SprintsContent() {
+    const [isShowProjectModal, setIsShowProjectModal] = useState(false);
+
+    const toggleProjectModal = () => setIsShowProjectModal(state => !state);
+
     return (
         <div className={styles.asideWrap}>
+            <AddButton onClick={toggleProjectModal} />
+
             <div className={styles.asideBack}>
                 <svg className={styles.backSvg}>
                     <use href={spriteGoBack + '#icon-goBack'}></use>
@@ -16,6 +25,15 @@ export default function SprintsContent() {
                     <span className={styles.asideSprintsName}>Project 1</span>
                 </li>
             </ul>
+
+            {isShowProjectModal && (
+                <NewItemModal
+                    title="Створення проекту"
+                    onClose={toggleProjectModal}
+                >
+                    <NewProjectForm onClose={toggleProjectModal} />
+                </NewItemModal>
+            )}
         </div>
     );
 }
